61adf27f404fe3edba97856eda582e436f167d24
Jo Matsumaru <hatunehimeka@outlook.jp>
2023-02-14 23:46:55 +0900

    最初のコミット

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  
+- var pagename='Device_Advice'
+
+doctype html
+
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
